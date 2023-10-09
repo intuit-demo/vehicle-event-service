1d9473805827e2fa5370d7173ba010d2994a0d54
Sunstreaker <suthagar.pp@gmail.com>
2023-10-09 13:56:57 +0530

    intial commit

---
+++ src/main/java/com/intuit/vehicleeventservice/VehicleEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.intuit.vehicleeventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleEventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleEventServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intuit/vehicleeventservice/VehicleEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.vehicleeventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleEventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
